7860438ab52afa02324e4cbff199a5444b3f8e3e
Andrea Fourel <andreafourel@hotmail.fr>
2021-12-17 18:39:39 +0100

    spelling fix&few changes on client demand

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-const header = document.querySelector('#header');
-const intro = document.querySelector('.intro');
 const navLinks = document.querySelectorAll('.nav-link');
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY >= (header.offsetHeight - 20)) {
-    header.classList.add('header-shadow');
-  } else if (window.scrollY < (header.offsetHeight - 20)) {
-    header.classList.remove('header-shadow')
-  }
-})
 
 navLinks.forEach(navLink => {
   navLink.addEventListener('click' , () => {
@@ -18,4 +8,3 @@ navLinks.forEach(navLink => {
     document.querySelector('.navbar-toggler').click();
   })
 })
-
